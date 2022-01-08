cd78d77b13940b16a9c4930be60a13b5b4b635c6
paulrthornton <paulrthornton@icloud.com>
2022-01-08 14:09:13 +0000

    Add: Spies

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,69 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import guru.springframework.sfgpetclinic.services.map.PetMapService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class VisitControllerTest {
+
+    @Mock
+    VisitService visitService;
+
+    @Spy //Mock
+    PetMapService petService;
+
+    @InjectMocks
+    VisitController visitController;
+
+    @Test
+    void loadPetWithVisit() {
+        // given
+        Map<String, Object> model = new HashMap<>();
+        Pet pet = new Pet(1l);
+        petService.save(pet);
+
+        given(petService.findById(anyLong())).willCallRealMethod();
+
+        // when
+        Visit visit = visitController.loadPetWithVisit(1L, model);
+
+        // then
+        assertThat(visit).isNotNull();
+        assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isEqualTo(1L);
+    }
+
+    @Test
+    void loadPetWithVisitWithStubbing() {
+        // given
+        Map<String, Object> model = new HashMap<>();
+        Pet pet = new Pet(1l);
+        petService.save(pet);
+
+        given(petService.findById(anyLong())).willReturn(pet);
+
+        // when
+        Visit visit = visitController.loadPetWithVisit(1L, model);
+
+        // then
+        assertThat(visit).isNotNull();
+        assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isEqualTo(1L);
+    }
+}
